240e878e423aba4ce17a4e2b4fd35d0abaf39ca5
QuentinHamonic <quentin.hamonic25@gmail.com>
2026-06-24 17:27:44 +0200

    TP 4

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int nombre = Integer.parseInt(chaine);
+
+        int a = 7;
+        int b = 15;
+        int maximum = Integer.max(a, b);
+        System.out.println(maximum);
+    }
+}
